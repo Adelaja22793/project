8d901ee6c4ae9eeb2bc3e96d7b77c87a3283c812
Unuero Potential <unueroavwerosuo@gmail.com>
2020-03-03 18:31:00 +0100

    Changed the contents of  Layout file to Welcome, Modified some forms in Students folder and added other forms to it, Added a Student Model folder and added classes to the folder

---
+++ SIWES_BSSL/Data/Students/Scaf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIWES_BSSL.Data.Students
+{
+    public class Scaf
+    {
+        public int Id { get; set; }
+        public string RefNo { get; set; }
+        public DateTime Scaf_Date { get; set; }
+        public DateTime Commence_Date { get; set; }
+        public DateTime Complete_Date { get; set; }
+        public int Duration { get; set; }
+        public int EmployerSuperSetupId { get; set; }
+        public int EmployerSupervisorSetupId { get; set; }
+        public string Remarks { get; set; }
+
+        //public Employers.EmployerSuperSetup EmployerSuperSetup { get; set; }
+        //public Employers.EmployerSupervisorSetup EmployerSupervisor { get; set; }
+    }
+}
---
+++ SIWES_BSSL/Data/Students/SupervisorVisit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIWES_BSSL.Data.Students
+{
+    public class SupervisorVisit
+    {
+        public int Id { get; set; }
+        public string RefNo { get; set; }
+        public DateTime DateVisited { get; set; }
+        public int StudentId { get; set; }
+        public string SupervisorRemarks1 { get; set; }
+        public string SupervisorRemark2 { get; set; }
+        public int NumberOfVisit { get; set; }
+        public int SupervisorId { get; set; }
+
+        //navigation properties
+        //public Student Student { get; set; }
+        //public IntSupervisor Supervisor { get; set; }
+    }
+}
