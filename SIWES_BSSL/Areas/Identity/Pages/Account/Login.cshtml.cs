e98eed6dc8a0029339471ef0287b732e814cedf9
Unuero Potential <unueroavwerosuo@gmail.com>
2020-03-05 12:28:07 +0100

    Edited By Seun

--- SIWES_BSSL/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SIWES_BSSL/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace Admin.Areas.Identity.Pages.Account
+namespace SIWES_BSSL.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
