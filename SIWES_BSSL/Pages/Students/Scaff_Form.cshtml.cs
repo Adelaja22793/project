860a5e1234fa50f18a6637bd9fe34f16c2b87b89
Unuero Potential <unueroavwerosuo@gmail.com>
2020-03-05 12:32:57 +0100

    Edited some works

---
+++ SIWES_BSSL/Pages/Students/Scaff_Form.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SIWES_BSSL
+{
+    public class Scaff_FormModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
