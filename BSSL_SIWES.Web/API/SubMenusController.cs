fcb7df43d25b2bf04030274deb690038a7e452f8
Unuero Potential <unueroavwerosuo@gmail.com>
2020-03-13 17:31:18 +0100

    Separated Data from the main

--- BSSL_SIWES.Web/API/SubMenusController.cs
+++ BSSL_SIWES.Web/API/SubMenusController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SiwesData.Data;
-using SiwesData.Data.Menu;
+using SiwesData;
+using SiwesData.Menu;
 
 namespace BSSL_SIWES.Web.API
 {
