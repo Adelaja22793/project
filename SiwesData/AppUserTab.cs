db2c511368a7d62f5b927a4eb6620522885aad18
Seun <Seun@DESKTOP-4OD04GF>
2020-03-23 13:01:23 +0100

    app user class that mimic identity user

---
+++ SiwesData/AppUserTab.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace SiwesData
+{
+   public class AppUserTab: IdentityUser
+    {
+      //  public 
+    }
+}
