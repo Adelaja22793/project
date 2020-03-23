1056c86691703a9048ac4b4b412b9856b608cc53
Charles Okoli <charlesokoli63@gmail.com>
2020-03-23 12:09:24 +0100

    Setup forms Update

---
+++ BSSL_SIWES.Web/Pages/Setup/InstitutionStudyLevel.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SiwesData.Data.Setup;
+
+namespace BSSL_SIWES.Web.Pages.Setup
+{
+    public class InstitutionStudyLevelModel : PageModel
+    {
+        private readonly SiwesData.Data.ApplicationDbContext _context;
+
+        [BindProperty]
+        public InstLevelStudy InstLevelStudy { get; set; }
+        public IList<InstLevelStudy> InstLevelStudyList { get; set; }
+
+        public InstitutionStudyLevelModel(SiwesData.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task OnGetAsync()
+        {
+            InstLevelStudyList = await _context.InstLevelStudy.ToListAsync();
+            ViewData["Institution"] = new SelectList(_context.Institution,"Id","Name");
+        }
+    }
+}
