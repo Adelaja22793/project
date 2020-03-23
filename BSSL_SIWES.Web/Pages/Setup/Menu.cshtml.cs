46e612e8f2b4fee2cf3025b15769db535c6af282
Seun <Seun@DESKTOP-4OD04GF>
2020-03-23 12:54:23 +0100

    Changes on menu and sub menus setup

--- BSSL_SIWES.Web/Pages/Setup/Menu.cshtml.cs
+++ BSSL_SIWES.Web/Pages/Setup/Menu.cshtml.cs
@@ -2,36 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace BSSL_SIWES.Web
 {
     public class MenuModel : PageModel
     {
         private readonly SiwesData.Data.ApplicationDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
-        public MenuModel(SiwesData.Data.ApplicationDbContext context,
-            UserManager<IdentityUser> userManager)
+
+        public MenuModel(SiwesData.Data.ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
         public List<SiwesData.Data.Menu.Menu> Menus { get; set; }
         [BindProperty]
         public SiwesData.Data.Menu.Menu Menu { get; set; }
-        public string MenuName { get; set; }
-        public string Message { get; set; }
+
         public async Task OnGetAsync()
         {
-            //Menus = _context.Menu.to 
             Menus = await _context.Menu.ToListAsync();
-            if (Menus != null)
+            if (Menus !=null)
             {
-                Message = "No Menu Has Been Setup, Please Click On Add Menu to Add";
+
             }
         }
+
+
+        //public async Task<IActionResult> OnPostAsync()
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Page();
+        //    }
+
+        //    _context.Menu.Add(Menu);
+        //    await _context.SaveChangesAsync();
+
+        //    return RedirectToPage("./Index");
+        //}
     }
 }
