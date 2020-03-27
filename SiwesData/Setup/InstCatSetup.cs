147d34688b15dd82c2eeacea753d759a61d21240
Charles Okoli <charlesokoli63@gmail.com>
2020-03-27 21:34:29 +0100

    model class update

--- SiwesData/Setup/InstCatSetup.cs
+++ SiwesData/Setup/InstCatSetup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,11 @@ namespace SiwesData.Setup
     public class InstCatSetup
     {
         public int Id { get; set; }
+        [Required]
         public string Code { get; set; }
+        [Required]
         public string Name { get; set; }
         public Boolean Deactivate { get; set; }
-        public ICollection<InstTypeSetup> InstTypes { get; set; }
+       // public ICollection<InstTypeSetup> InstTypes { get; set; }
     }
 }
