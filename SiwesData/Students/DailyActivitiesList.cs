905980ea52d82fc9a496c4e1c9fe2a5a1af7d332
Unuero Potential <unueroavwerosuo@gmail.com>
2020-04-05 23:04:01 +0100

    \working on student

--- SiwesData/Students/DailyActivitiesList.cs
+++ SiwesData/Students/DailyActivitiesList.cs
@@ -8,16 +8,16 @@ namespace SiwesData.Students
     {
         public int Id { get; set; }
         public int DailyActivitiesId { get; set; }
-        public DateTime Day1 { get; set; }
-        public string Day1Description { get; set; }
-        public DateTime Day2 { get; set; }
-        public string Day2Description { get; set; }
-        public DateTime Day3 { get; set; }
-        public string Day3Description { get; set; }
-        public DateTime Day4 { get; set; }
-        public string Day4Description { get; set; }
-        public DateTime Day5 { get; set; }
-        public string Day5Description { get; set; }
+        public DateTime DayDate { get; set; }
+        public string DayDescription { get; set; }
+        public string  WeekDayName { get; set; }
+        //public string Day2Description { get; set; }
+        //public DateTime Day3 { get; set; }
+        //public string Day3Description { get; set; }
+        //public DateTime Day4 { get; set; }
+        //public string Day4Description { get; set; }
+        //public DateTime Day5 { get; set; }
+        //public string Day5Description { get; set; }
 
         public DailyActivities DailyActivities { get; set; }
     }
