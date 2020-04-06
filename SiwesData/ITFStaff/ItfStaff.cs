a00159d85283497098d01ea1b9253dccda198370
Oluwaseun Ajayi <seunnext2u@outlook.com>
2020-04-06 08:15:00 +0100

    it staff class

---
+++ SiwesData/ITFStaff/ItfStaff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiwesData.ITFStaff
+{
+  public  class ItfStaff
+    {
+        public int Id { get; set; }
+        public string StaffId { get; set; }
+        public string Surname { get; set; }
+        public string OtherNames { get; set; }
+        public string Gradelvl { get; set; }
+        public string OfficeCode { get; set; }
+
+        public string Email { get; set; }
+        public Boolean Suspended { get; set; }
+
+    }
+}
