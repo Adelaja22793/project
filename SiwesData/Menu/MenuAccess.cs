ab31085dc2b6f75cb00781d975384ab78419af66
Oluwaseun Ajayi <seunnext2u@outlook.com>
2020-04-13 13:28:48 +0100

    Seun menu update

--- SiwesData/Menu/MenuAccess.cs
+++ SiwesData/Menu/MenuAccess.cs
@@ -1,13 +1,13 @@
-﻿using SiwesData.Setup;
+﻿
+using SiwesData.Data.Menu;
+using SiwesData.Data.Setup;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SiwesData.Menu
+namespace SiwesData.Data.Menu
 {
-    //[Table("SubMenu")]
     public class MenuAccess
     {
         public int Id { get; set; }
@@ -17,9 +17,7 @@ namespace SiwesData.Menu
         //public int MenuId { get; set; }
 
         //public Menu Menu { get; set; }   
-        //public SubMenu SubMenu { get; set; }
-        public Menu Menu { get; set; }
         public RoleTb RoleTb { get; set; }
-        public bool Active { get; set; }
+        public SubMenu SubMenu { get; set; }
     }
 }
