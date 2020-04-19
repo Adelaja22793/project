743cb516807b26383f9373e00a6f2db9323b62ab
Unuero Potential <unueroavwerosuo@gmail.com>
2020-04-19 04:38:37 +0100

    Created and Updated Some APIs to handle some works

---
+++ BSSL_SIWES.Web/API/Courses_API/NewEmployerRequestController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiwesData;
+using SiwesData.Setup;
+
+namespace BSSL_SIWES.Web.API.Courses_API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewEmployerRequestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewEmployerRequestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/NewEmployerRequest
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NewCourseRequest>>> GetNewCourseRequests()
+        {
+            return await _context.NewCourseRequests.ToListAsync();
+        }
+
+        // GET: api/NewEmployerRequest/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NewCourseRequest>> GetNewCourseRequest(int id)
+        {
+            var employerSuperSetup = await _context.NewCourseRequests.Include(x => x.AreaOffice)
+                .Include(x => x.LGA).Include(x => x.BusinessLine)
+                .Where(x => x.AreaOffice.Id == x.AreaOfficeId && x.BusinessLine.Id == x.BusinessLineId && x.LGAId == x.LGA.Id)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employerSuperSetup == null)
+            {
+                return NotFound();
+            }
+
+            return employerSuperSetup;
+        }
+
+        // PUT: api/NewEmployerRequest/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNewCourseRequest(int id, NewCourseRequest newCourseRequest)
+        {
+            if (id != newCourseRequest.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(newCourseRequest).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NewCourseRequestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/NewEmployerRequest
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<NewCourseRequest>> PostNewCourseRequest(NewCourseRequest newCourseRequest)
+        {
+            try
+            {
+
+                var newCourseRecom = new NewCourseRequest
+                {
+                    Name = newCourseRequest.Name,
+                    Approved = newCourseRequest.Approved = false,
+                    DateIn = newCourseRequest.DateIn = DateTime.Now,
+                    InstitiutionId = newCourseRequest.InstitiutionId,
+                    ReqstType = newCourseRequest.ReqstType,
+                    Code = newCourseRequest.Code,
+
+                    Address1 = newCourseRequest.Address1,
+                    Address2 = newCourseRequest.Address2,
+                    LGAId = newCourseRequest.LGAId,
+                    CoporationType = newCourseRequest.CoporationType,
+                    YearOfIncop = newCourseRequest.YearOfIncop,
+                    BusinessLineId = newCourseRequest.BusinessLineId,
+
+                    BusinessType = newCourseRequest.BusinessType,
+                    AreaOfficeId = newCourseRequest.AreaOfficeId,
+                    WebAddress = newCourseRequest.WebAddress,
+                    Email = newCourseRequest.Email,
+                    PhoneNo = newCourseRequest.PhoneNo,
+                    PhoneNo2 = newCourseRequest.PhoneNo2,
+                };
+                _context.NewCourseRequests.Add(newCourseRecom);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("PostNewCourseRequest", new { id = newCourseRecom.Id }, newCourseRecom);
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(500, $"{newCourseRequest.Name} HAS BEEN SENT FOR SETUP, PREVIOUSLY, PLEASE CHECK!!!");
+            }
+        }
+
+        // DELETE: api/NewEmployerRequest/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<NewCourseRequest>> DeleteNewCourseRequest(int id)
+        {
+            var newCourseRequest = await _context.NewCourseRequests.FindAsync(id);
+            if (newCourseRequest == null)
+            {
+                return NotFound();
+            }
+
+            _context.NewCourseRequests.Remove(newCourseRequest);
+            await _context.SaveChangesAsync();
+
+            return newCourseRequest;
+        }
+
+        private bool NewCourseRequestExists(int id)
+        {
+            return _context.NewCourseRequests.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BSSL_SIWES.Web/API/Student/ExportExcelController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiwesData;
+using SiwesData.Students;
+
+namespace BSSL_SIWES.Web.API.Student
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportExcelController : ControllerBase
+    {
+        //private readonly ApplicationDbContext _context;
+
+        //public ExportExcelController(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+        //private List<StudentSetUp> GetDataFromCSVFile(Stream stream)
+        //{
+        //    var empList = new List<StudentSetUp>();
+        //    try
+        //    {
+        //        using (var reader = ExcelReaderFactory.CreateCsvReader(stream))
+        //        {
+        //            var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration
+        //            {
+        //                ConfigureDataTable = _ => new ExcelDataTableConfiguration
+        //                {
+        //                    UseHeaderRow = true // To set First Row As Column Names  
+        //                }
+        //            });
+
+        //            if (dataSet.Tables.Count > 0)
+        //            {
+        //                var dataTable = dataSet.Tables[0];
+        //                foreach (DataRow objDataRow in dataTable.Rows)
+        //                {
+        //                    if (objDataRow.ItemArray.All(x => string.IsNullOrEmpty(x?.ToString()))) continue;
+        //                    empList.Add(new StudentSetUp()
+        //                    {
+        //                        Id = Convert.ToInt32(objDataRow["ID"].ToString()),
+        //                        MatricNumber = objDataRow["MatricNumber"].ToString(),
+        //                        MatricYear = objDataRow["MatricYear"].ToString(),
+        //                        Surname = objDataRow["Surname"].ToString(),
+        //                        OtherNames = objDataRow["OtherName"].ToString(),
+        //                        YearOfStudy = objDataRow["YearOfStudy"].ToString(),
+        //                        PhoneNo = objDataRow["PhoneNo"].ToString(),
+        //                        Email = objDataRow["Email"].ToString(),
+        //                        BatchNo = objDataRow["BatchNo"].ToString(),
+        //                        SiwesYear = objDataRow["SiwesYear"].ToString(),
+        //                    });
+        //                }
+        //            }
+
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+
+        //    return empList;
+        //}
+
+        //public static class Extensions
+        //{
+        //    public static DataTable ToDataTable<T>(this List<T> items)
+        //    {
+        //        DataTable dataTable = new DataTable(typeof(T).Name);
+
+        //        //Get all the properties  
+        //        PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        //        foreach (PropertyInfo prop in Props)
+        //        {
+        //            //Defining type of data column gives proper data table   
+        //            var type = (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) ? Nullable.GetUnderlyingType(prop.PropertyType) : prop.PropertyType);
+        //            //Setting column names as Property names  
+        //            dataTable.Columns.Add(prop.Name, type);
+        //        }
+        //        foreach (T item in items)
+        //        {
+        //            var values = new object[Props.Length];
+        //            for (int i = 0; i < Props.Length; i++)
+        //            {
+        //                //inserting property values to datatable rows  
+        //                values[i] = Props[i].GetValue(item, null);
+        //            }
+        //            dataTable.Rows.Add(values);
+        //        }
+        //        return dataTable;
+        //    }
+        //}
+
+        //// GET: api/ExportExcel
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/ExportExcel/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/ExportExcel
+        //[HttpPost]
+        //public async Task<ActionResult> ImportFile(HttpPostedFileBase importFile)
+        //{
+        //    if (importFile == null) return Json(new { Status = 0, Message = "No File Selected" });
+
+        //    try
+        //    {
+        //        var fileData = GetDataFromCSVFile(importFile.InputStream);
+
+        //        var dtEmployee = fileData.ToDataTable();
+        //        var tblEmployeeParameter = new SqlParameter("tblEmployeeTableType", SqlDbType.Structured)
+        //        {
+        //            TypeName = "dbo.tblTypeEmployee",
+        //            Value = dtEmployee
+        //        };
+        //        await _context.Database.ExecuteSqlCommandAsync("EXEC spBulkImportEmployee @tblEmployeeTableType", tblEmployeeParameter);
+        //        return Json(new { Status = 1, Message = "File Imported Successfully " });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Json(new { Status = 0, Message = ex.Message });
+        //    }
+        //}
+
+        //// PUT: api/ExportExcel/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
