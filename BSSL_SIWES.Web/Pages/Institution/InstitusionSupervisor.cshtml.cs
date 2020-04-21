e19965047a44f0dc663c8a4ab90c5a74d3158759
Unuero Potential <unueroavwerosuo@gmail.com>
2020-04-21 10:33:30 +0100

    Removing all Outcoded ID's in Institutions

--- BSSL_SIWES.Web/Pages/Institution/InstitusionSupervisor.cshtml.cs
+++ BSSL_SIWES.Web/Pages/Institution/InstitusionSupervisor.cshtml.cs
@@ -9,31 +9,44 @@ using Microsoft.EntityFrameworkCore;
 using SiwesData.Setup;
 using SiwesData.Students;
 using SiwesData;
+using BSSL_SIWES.Web.Areas.Identity.Pages.Account;
+using Microsoft.Extensions.Logging;
+
 namespace BSSL_SIWES.Web
 {
     public class InstitusionSupervisorModel : PageModel
     {
         private readonly SiwesData.ApplicationDbContext _context;
         private readonly UserManager<AppUserTab> _userManager;
+        private readonly SignInManager<AppUserTab> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
 
         public InstitusionSupervisorModel(SiwesData.ApplicationDbContext context,
-            UserManager<AppUserTab> userManager)
+            UserManager<AppUserTab> userManager, ILogger<LoginModel> logger,
+            SignInManager<AppUserTab> signInManager)
         {
             _context = context;
             _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
         }
         public IList<StudentSetUp> StudentSetUp { get; set; }
         public InstitutionOfficer InstitutionOfficer { get; set; }
-        public async Task<IActionResult> OnGetAsync(int id)
+        public async Task<IActionResult> OnGetAsync(int InstitutionOfficerId)
         {
-            id = 1;
-            if (id < 0)
+            //id = 1;
+            var loginUser = _userManager.GetUserId(User);
+            //var userEmail = await _userManager.GetUserNameAsync(loginUser);
+            var userEmail = _userManager.GetUserName(User);
+
+            InstitutionOfficerId = await _context.InstitutionOfficers.Where(x => x.Email == userEmail).Select(x => x.Id).FirstOrDefaultAsync();
+            if (InstitutionOfficerId < 0)
             {
                 return RedirectToPage("./InstitusionSupervisor");
             }
-            InstitutionOfficer = await _context.InstitutionOfficers.FindAsync(id = 3);
+            //InstitutionOfficerId = await _context.InstitutionOfficers.FindAsync(id = 3);
             StudentSetUp = await _context.StudentSetUps.Include(x => x.Courses).Include(x => x.InstitutionOfficer).ThenInclude(x => x.Institution)
-                .Where(x => x.Suspended == false && x.InstitutionOfficerId == 3)
+                .Where(x => x.Suspended == false && x.InstitutionOfficerId == InstitutionOfficerId)
                 .ToListAsync();
 
             if (StudentSetUp == null)
--- BSSL_SIWES.Web/Pages/Institution/SupervisorList.cshtml.cs
+++ BSSL_SIWES.Web/Pages/Institution/SupervisorList.cshtml.cs
@@ -23,7 +23,7 @@ namespace BSSL_SIWES.Web.Pages.Institution
         public IList<StudentSetUp> ListOfStudent { get; set; }
         public async Task OnGetAsync(int id)
         {
-            id = 19;
+            //id = 19;
             ListOfStudent = await _context.StudentSetUps.Include(x => x.Courses).Include(x => x.InstitutionOfficer).ThenInclude(x => x.Institution)
                 .Where(x => x.InstitutionOfficerId == id && x.CoursesId == x.Courses.Id && x.Suspended == false).ToListAsync();
         }
